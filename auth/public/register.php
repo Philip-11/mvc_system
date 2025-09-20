fe66f8269a1482ffb68fd3d94a5c42188e2a3eff
Philip-11 <magsaysayphil@gmail.com>
2025-09-20 18:40:19 +0800

    Added database connection and bootstrap

--- auth/public/register.php
+++ auth/public/register.php
@@ -1,5 +1,6 @@
 <?php require_once __DIR__ . '/../../bootstrap.php';
 
+require_once BASE_PATH . '/config/database.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,34 +13,37 @@
 </head>
 
 <body>
-    <form action="POST">
-        <div>Sign up</div>
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" name="" id="">
-        </div>
+    <main class="container">
+        <form action="POST">
+            <div>Sign up</div>
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" name="" id="">
+            </div>
 
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="" id="">
-        </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" name="" id="">
+            </div>
 
-        <div>
-            <label for="password2">Password Again:</label>
-            <input type="password" name="" id="">
-        </div>
+            <div>
+                <label for="password2">Password Again:</label>
+                <input type="password" name="" id="">
+            </div>
 
-        <div>
-            <label for="agree">
-                <input type="checkbox" name="agree" id="agree" value="yes"> I agree with the <a href="">terms of services</a>
-            </label>
-        </div>
-        <button type="submit">Register</button>
-        <footer>Already a member? <a href="login.php">login here!</a></footer>
+            <div>
+                <label for="agree">
+                    <input type="checkbox" name="agree" id="agree" value="yes"> I agree with the <a href="">terms of services</a>
+                </label>
+            </div>
+            <button type="submit">Register</button>
+            <footer>Already a member? <a href="login.php">login here!</a></footer>
 
 
 
-    </form>
+        </form>
+    </main>
+
 </body>
 
 </html>
---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+class Database
+{
+    private const DB_HOST = "localhost";
+    private const DB_NAME = "users";
+    private const DB_USER = "root";
+    private const DB_PASS = "";
+
+    static function connect(): PDO
+    {
+        static $conn;
+
+        if (!$conn) {
+            $dsn = "mysql:host=" . self::DB_HOST . ";dbname=" . self::DB_NAME . ";charset=utf8mb4";
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ];
+
+            $conn = new PDO($dsn, SELF::DB_USER, SELF::DB_PASS, $options);
+        }
+
+        return $conn;
+    }
+}
