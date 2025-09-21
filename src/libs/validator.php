7f30baa5791a1e8c9a0fb21549590941ac057d79
Philip-11 <magsaysayphil@gmail.com>
2025-09-22 02:06:39 +0800

    Added validator class

---
+++ src/libs/validator.php
@@ -0,0 +1,39 @@
+<?php
+
+class Validator
+{
+    public static function username($username)
+    {
+        if (empty($username)) {
+            return "Username is required";
+        } elseif (!preg_match("/^[a-zA-Z0-9_]+$/", $username)) {
+            return "Username must only contain letters, numbers and underscores";
+        } elseif (strlen($username) < 3) {
+            return "Username must be above 3 characters";
+        }
+
+        return null;
+    }
+
+    public static function email($email)
+    {
+        if (empty($email)) {
+            return "Email is required";
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return "Invalid email format";
+        }
+
+        return null;
+    }
+
+    public static function password($password, $minLength = 8)
+    {
+        if (empty($password)) {
+            return "Password is required";
+        } elseif (strlen($password) < $minLength) {
+            return "Password must be at least $minLength characters long";
+        }
+
+        return null;
+    }
+}
